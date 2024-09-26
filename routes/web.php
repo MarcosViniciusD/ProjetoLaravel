b833c3f5b4865a7094376a67ed5d460e50f3c6d6
Marcos Vinicius <Marcos.duarte1501@gmail.com>
2024-09-26 14:47:20 -0300

    Inicio do Projeto

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ProdutoController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [ProdutoController::class, 'index'])->name('produto.index');
+
+Route::get('/produto/{id?}', [ProdutoController::class, 'show'])->name('produto.show');
